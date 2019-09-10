5286fe0eafc7ad58bfd76ebd7f3316ef2d98970f
Tyler Berrett <btylerberrett@gmail.com>
2019-09-10 13:30:20 -0600

    initial commit

--- M02/GuidedProject/AbstractionIntPoly/app/src/main/java/com/lambdaschool/abstractionintpoly/ItemDetailFragment.kt
+++ M02/GuidedProject/AbstractionIntPoly/app/src/main/java/com/lambdaschool/abstractionintpoly/ItemDetailFragment.kt
@@ -1,5 +1,6 @@
 package com.lambdaschool.abstractionintpoly
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -36,6 +37,7 @@ class ItemDetailFragment : Fragment() {
                 val dff = it.getSerializable(ARG_ITEM_ID) as SwApiObject
                 activity?.toolbar_layout?.let { layout ->
                     // TODO 11: S05M02-13 set up the item from the object
+                    layout.title = item?.name
                 }
             }
         }
@@ -47,12 +49,38 @@ class ItemDetailFragment : Fragment() {
     ): View? {
         val rootView = inflater.inflate(R.layout.item_detail, container, false)
 
+        rootView.send_info.setOnClickListener { responseObject?.provideInfoForObject(item?.info() ?: "No info") }
+
         // TODO 10: Set up the drawable from the item
+        item?.let {
+            rootView.item_image.setImageDrawable(
+                rootView.context.getDrawable(
+                    DrawableResolver.getDrawableId(it.category, it.id)
+                )
+            )
+        }
 
         return rootView
     }
 
     // TODO 13: S05M02-15 Create your own interface to communicate with the Activity
+    interface DetailResponse {
+        fun provideInfoForObject(info: String)
+    }
+
+    private var responseObject: DetailResponse? = null
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is DetailResponse) {
+            responseObject = context
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        responseObject = null
+    }
 
     // TODO 16: Store the Activity in a property in onAttach
 
